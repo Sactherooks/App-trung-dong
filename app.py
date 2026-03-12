6e46207d1b4baaf317b6d41361045277a4160403
Ford1611 <lamford1611@gmail.com>
2026-03-12 14:25:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("abc")
+import pygame
+from pygame.locals import *
+pygame.init()
+screen_width = 1000
+screen_height = 1000
+screen = pygame.display.set_mode((screen_width,screen_height))
+pygame.display.set_caption('ehehhehehh')
+title_size = 50
+bg_image = pygame.image.load()
